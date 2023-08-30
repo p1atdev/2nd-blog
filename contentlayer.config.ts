3520fbac8a624f1f5867246a2c6d91328f0abc0e
Plat <git@p1at.dev>
2023-08-30 12:57:08 +0900

    chore: sort order settings and set up contentlayer

---
+++ contentlayer.config.ts
@@ -0,0 +1,23 @@
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+
+export const Post = defineDocumentType(() => ({
+  name: "Post",
+  filePathPattern: `**/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: { type: "string", required: true },
+    date: { type: "date", required: true },
+    slug: { type: "string", required: true },
+  },
+  computedFields: {
+    url: {
+      type: "string",
+      resolve: (post) => `/src/${post._raw.flattenedPath}`,
+    },
+  },
+}));
+
+export default makeSource({
+  contentDirPath: "src/posts",
+  documentTypes: [Post],
+});
