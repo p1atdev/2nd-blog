c51f619e5d0ec8759de37ab5153b757873f5da74
Plat <git@p1at.dev>
2023-09-01 15:58:02 +0900

    not to track styled-system

--- styled-system/css/css.d.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-import type { SystemStyleObject } from '../types/index';
-
-interface CssFunction {
-  (...styles: SystemStyleObject[]): string
-  raw: (styles: SystemStyleObject) => SystemStyleObject
-}
-
-export declare const css: CssFunction;
--- styled-system/types/index.d.ts
+++
@@ -1,4 +0,0 @@
-/* eslint-disable */
-import './global.d.ts'
-export type { ConditionalValue } from './conditions';
-export type { GlobalStyleObject, JsxStyleProps, SystemStyleObject } from './system-types';
