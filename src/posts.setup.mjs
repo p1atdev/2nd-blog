c80566a318142a442b316928bbfd78e87bc7a2f5
Plat <git@p1at.dev>
2023-09-04 11:34:07 +0900

    fix: clone manually instead of submodule

--- src/posts.setup.mjs
+++ src/posts.setup.mjs
@@ -4,45 +4,20 @@ import { env } from "process";
 
 import { simpleGit } from "simple-git";
 
+const postsDir = path.resolve("src", "posts");
+
 const sourceDir = path.resolve("src", "posts", "assets");
 const destDir = path.resolve("public", "assets");
 
 console.log("Copying images to public folder...");
 
-// 行儀が悪いけど、vercelのときだけurlを書き換えてトークンを差し込む
-const configureSubmoduleUrl = () => {
-  const GITHUB_ASSETS_KEY = env.GITHUB_ASSETS_KEY; // set in vercel env var
-  if (!GITHUB_ASSETS_KEY) {
+const cloneSubmoduleRepo = async () => {
+  const GITHUB_ASSETS_REPO_URL = env.GITHUB_ASSETS_REPO_URL; // set in vercel env var
+  if (!GITHUB_ASSETS_REPO_URL) {
     return;
   }
 
-  // console.log("GITHUB_ASSETS_KEY:", GITHUB_ASSETS_KEY);
-
-  // .gitmodules の url を書き換える
-  const gitmodulesPath = path.resolve(".gitmodules");
-  const gitmodules = fs.readFileSync(gitmodulesPath, "utf-8");
-  const newGitmodules = gitmodules.replace(
-    "https://github.com",
-    `https://${GITHUB_ASSETS_KEY}@github.com`
-  );
-  fs.writeFileSync(gitmodulesPath, newGitmodules, "utf-8");
-};
-
-const submoduleInit = async () => {
-  const GIT_USERNAME = env.GIT_USERNAME;
-  const GIT_EMAIL = env.GIT_EMAIL;
-
-  if (GIT_USERNAME && GIT_EMAIL) {
-    await simpleGit().addConfig("user.name", GIT_USERNAME);
-    await simpleGit().addConfig("user.email", GIT_EMAIL);
-  }
-
-  await simpleGit().submoduleUpdate(["--init", "--recursive"], (err, data) => {
-    if (err) {
-      throw err;
-    }
-    console.log("Submodule updated");
-  });
+  await simpleGit().clone(GITHUB_ASSETS_REPO_URL, postsDir);
 };
 
 const copyAssets = () => {
@@ -76,8 +51,7 @@ const copyAssets = () => {
 };
 
 const main = async () => {
-  configureSubmoduleUrl();
-  await submoduleInit();
+  await cloneSubmoduleRepo();
   copyAssets("content/posts");
 };
 
