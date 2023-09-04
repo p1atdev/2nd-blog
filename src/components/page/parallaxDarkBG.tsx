6b94a58f29ab1de821ad9765a62af0b184d99760
Plat <git@p1at.dev>
2023-09-04 19:10:30 +0900

    feat: lazy image loading with animation using framer motion

--- src/components/page/parallaxDarkBG.tsx
+++ src/components/page/parallaxDarkBG.tsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
+
 import Image from "next/image";
+
+import { motion } from "framer-motion";
 
 import { css } from "@panda/css";
 
 import MouseParallax from "../common/mouseParallax";
 
 export default function ParallaxDarkBG() {
+  // ref: https://prototypr.io/post/using-framer-motion-to-create-smooth-lazy-load-image-effects
+  const [imageLoading, setImageLoading] = useState(true);
+
+  const imageLoaded = () => {
+    setImageLoading(false);
+  };
+
   return (
     <MouseParallax
       className={css({
@@ -19,11 +30,20 @@ export default function ParallaxDarkBG() {
         h: "100svh",
       })}
     >
-      <figure
+      <motion.figure
         className={css({
           bg: "home.black",
           zIndex: 0,
         })}
+        initial={{ opacity: 0 }}
+        animate={{
+          opacity: imageLoading ? 0 : 1,
+        }}
+        transition={{
+          height: { delay: 0, duration: 0.4 },
+          opacity: { delay: 0.5, duration: 0.4 },
+        }}
+        onLoad={imageLoaded}
       >
         <Image
           className={css({
@@ -33,7 +53,7 @@ export default function ParallaxDarkBG() {
           alt="illustraion of a girl as a background image"
           fill
         />
-      </figure>
+      </motion.figure>
     </MouseParallax>
   );
 }
